7801119dd89357139166df520faed9e539a3dc79
siawli <tiosiawlilili@gmail.com>
2022-05-21 15:25:43 +0800

    test

--- src/test/java/MyFitnessJourney/VTTP/Project/Fitness/RecipesControllerTest.java
+++ src/test/java/MyFitnessJourney/VTTP/Project/Fitness/RecipesControllerTest.java
@@ -10,10 +10,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 
